8deb46aadb1388c9b33056be9f6f3db53211dea6
lsp2610 <louis.porter26@gmail.com>
2024-08-13 11:59:52 +0100

    completed project

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+let password1El = document.getElementById("password1-el")
+let password2El = document.getElementById("password2-el")
+
+
+function generatePassword() {
+    let randomPassword = ""
+    for (let i = 0; i < 15; i++) {
+        randomPassword += characters[Math.floor(Math.random() * characters.length)]
+    }
+    return randomPassword
+}
+
+function generateRandomPasswords() {
+    password1El.textContent = generatePassword()
+    password2El.textContent = generatePassword()
+}
 
 
 
